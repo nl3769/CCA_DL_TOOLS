197ef93aaf4067af0cd91f25f3f04053c749a068
Nolann Laine <laine@linux1.dg.creatis.insa-lyon.fr>
2022-08-01 09:59:50 +0200

    minor update

---
+++ SIMULATION/run_local/run_beamforming_folder.m
@@ -0,0 +1,42 @@
+close all;
+clearvars;
+clc;
+
+set(0, 'DefaultFigureWindowStyle', 'docked');
+
+% --- add path
+addpath(fullfile('..', 'mtl_cores/'));
+addpath(fullfile('..', 'mtl_utils'));
+
+% --- path to results
+path_res01='/home/laine/cluster/PROJECTS_IO/SIMULATION/SEQ_MEIBURGER/';
+patients = list_files(path_res01);
+
+
+% for idp=21:1:length(patients)
+for idp=24:1:length(patients)
+%parfor idp=21:1:length(patients)
+    files = list_files(fullfile(path_res01, patients{idp}));
+    
+    for id=1:1:length(files)
+        pres_ = fullfile(path_res01, patients{idp}, files{id});
+        pres_
+        fct_run_image_reconstruction(pres_);
+    end
+end
+    
+% -------------------------------------------------------------------------
+function [files] = list_files(path)
+
+    listing = dir(path);
+    incr=1;
+    
+    for id=1:1:length(listing)
+        if listing(id).name ~= '.'
+            files{incr} = listing(id).name;
+            incr=incr+1;
+        end
+    end
+
+end
+% -------------------------------------------------------------------------
