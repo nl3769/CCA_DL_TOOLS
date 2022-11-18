e0efdf7491fc06a92131b538ad4b0b51a9501f74
laine <nolann.laine@outlook.fr>
2022-11-18 15:35:18 +0100

    remove parameters

--- caroDeepSeg/package_parameters/set_parameters_training_seg_template.py
+++ caroDeepSeg/package_parameters/set_parameters_training_seg_template.py
@@ -13,25 +13,26 @@ import package_utils.fold_handler                               as fh
 def setParameters():
 
   p = Parameters(
-        PDATA='/home/laine/PROJECTS_IO/CARODEEPFLOW/SEGMENTATION/DATA',                  # PATH TO LOAD DATA
-        PRES='/home/laine/PROJECTS_IO/CARODEEPFLOW/SEGMENTATION/TRN',                           # PATH TO SAVE TRAINING RESULTS
-        PSPLIT='/home/laine/PROJECTS_IO/CARODEEPFLOW/SEGMENTATION/SPLIT_PATIENT/fold_7',
+        PDATA='/home/laine/PROJECTS_IO/CARODEEPSEG/CUBS_DATABASE',                # PATH TO LOAD DATA
+        PRES='/home/laine/PROJECTS_IO/CARODEEPSEG/TRAINING',                      # PATH TO SAVE TRAINING RESULTS
+        PSPLIT='/home/laine/PROJECTS_IO/CARODEEPSEG/SPLIT_PATIENT/fold_0',
         IN_VIVO=True,
         LEARNING_RATE=0.001,
-        BATCH_SIZE=16,  # size of a batch
-        NB_EPOCH=500,
+        BATCH_SIZE=2,           # size of a batch
+        NB_EPOCH=15,
         VALIDATION=True,
-        DROPOUT=0.1,  # dropout during training
+        DROPOUT=0.1,            # dropout during training
         WORKERS=4,
         USER='LAINE',
-        EXPNAME='SEG_TRAINING_FOLD_7',
-        DEVICE='cuda',                      # cuda/cpu
+        EXPNAME='SEG_TRAINING_FOLD_0',
+        DEVICE='cuda',          # cuda/cpu
         RESTORE_CHECKPOINT=True,
-        # --- If feature is split, then chose parameters for Unet
+        ENTITY='nl37',
+        # --- parameters for unet
         KERNEL_SIZE=(3, 3),
         PADDING=(1, 1),
         USE_BIAS=True,
-        NGF=32,  # number of input features of the Unet
+        NGF=32,                 # number of input features of the Unet
         NB_LAYERS=4
   )
 
