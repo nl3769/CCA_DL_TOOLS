e91a7a96beb58c9e9d8f4ffbbd2a653011e33331
nl3769 <60877526+nl3769@users.noreply.github.com>
2022-12-01 15:10:23 +0100

    Update cluster_full_pipeline.sh

--- SIMULATION/run_cluster/shell/cluster_full_pipeline.sh
+++ SIMULATION/run_cluster/shell/cluster_full_pipeline.sh
@@ -22,14 +22,7 @@ echo '""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""'
 SIMULATION=$(qsub -N $4 -v path_param=$1,log_name=$4,path_phantom=$2,id_tx=$5 pbs/simulation.pbs)
 echo $SIMULATION
 
-# --- CLUSTER RF DATA
-cls_RF_name="$4_CLS"
-CLUSTER_RF=$(qsub -N $cls_RF_name -v path_RF=$6,log_name=$4 -W depend=afterokarray:$SIMULATION pbs/cluster_RF.pbs)
-#CLUSTER_RF=$(qsub -N $cls_RF_name -v path_RF=$6,log_name=$4 pbs/cluster_RF.pbs)
-echo $CLUSTER_RF
-
-
 # --- BEAMFORM DATA
 bf_name="$4_BF"
-BEAMFORMING=$(qsub -N $bf_name -v log_name=$4,pres=$7 -W depend=afterany:$CLUSTER_RF pbs/beamforming.pbs)
+BEAMFORMING=$(qsub -N $bf_name -v log_name=$4,pres=$7 -W depend=afterokarray:$SIMULATION pbs/beamforming.pbs)
 echo $BEAMFORMING
