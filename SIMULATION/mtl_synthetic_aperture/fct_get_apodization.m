8e926e45e455f911494abd5534e062f55d4c715d
laine <nolann.laine@outlook.fr>
2022-12-01 11:46:59 +0100

    correct bug in apodization

--- SIMULATION/mtl_synthetic_aperture/fct_get_apodization.m
+++ SIMULATION/mtl_synthetic_aperture/fct_get_apodization.m
@@ -35,7 +35,7 @@ function [apod_window] = apod_hanning_full(dim, Nactive)
     for id_tx=1:1:width
 
         if id_tx >=Nactive/2 && id_tx < (width - Nactive/2 +1)
-            id = floor(width - Nactive +1);
+            id = floor(id_tx - Nactive/2 +1);
             apod_window(:, id:id+Nactive-1, id_tx) = apod_window_;
         end
 
@@ -45,6 +45,54 @@ end
 
 % ---------------------------------------------------------------------------------------
 function [apod_window] = apod_hanning_adaptative(dim, Nactive, pitch, f_number, dz)
+
+    width = dim(2);
+    height = dim(1);
+
+    apod_window_ = zeros([height, Nactive]);
+    apod_window = zeros([height, width, width]);
+    x_probe = -(Nactive-1)*pitch/2:pitch:(Nactive-1)*pitch/2;
+    
+    % --- Create apodization window of size depth*Nelements
+    for id_z=1:1:height
+        z = id_z*dz;
+        Nactive_ = round(z/(pitch*f_number));
+
+        if Nactive_ < 3 
+            Nactive_ = 3 ; 
+        elseif Nactive_ >= Nactive 
+            Nactive_ = Nactive;
+        end
+
+        apod_act_int = hanning(Nactive_*10)';
+        x_act_int = -(Nactive_*10-1)*pitch/10/2 : pitch/10 : (Nactive_*10-1)*pitch/10/2;
+
+        x_int = -(Nactive * 10 - 1) * pitch/10/2 : pitch/10 : (Nactive * 10 - 1) * pitch/10/2;
+        apod_int = interp1(x_act_int, apod_act_int, x_int, 'linear', 0);
+
+        apod = interp1(x_int, apod_int, x_probe);
+        
+        % --- sum has to equal equal to zero
+        sum_ = sum(apod);
+        
+        apod_window_(id_z, :) = apod/sum_;
+
+    end          
+
+    % --- window offset according to the element
+    for id_tx=1:1:width
+
+        if id_tx > Nactive/2 && id_tx < (width - Nactive/2 +1)
+            id = floor(id_tx - Nactive/2 +1);
+            apod_window(:, id:id+Nactive-1, id_tx) = apod_window_;
+        end
+
+    end
+    
+end
+
+% ---------------------------------------------------------------------------------------
+function [apod_window] = apod_hanning_adaptative_(dim, Nactive, pitch, f_number, dz)
 
     width = dim(2);
     height = dim(1);
