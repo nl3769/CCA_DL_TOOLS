24820af2a2b5d9b0627b87f059611e67758dee49
laine <nolann.laine@outlook.fr>
2023-02-21 09:14:14 +0100

    rename files

--- caroDeepSeg/run_local/run_database.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-source ~/venv/pytorch/bin/activate
-WD=/home/laine/cluster/REPOSITORIES/CCA_DL_TOOLS/caroDeepSeg
-PYTHONPATH=$WD python package_core/run_database.py -param set_parameters_database.py
--- caroDeepSeg/run_local/run_training_seg.sh
+++ caroDeepSeg/run_local/run_training_seg.sh
@@ -3,6 +3,6 @@
 
 source ~/venv/pytorch/bin/activate
 
-WD=/home/laine/Documents/REPO/CCA_DL_TOOLS/caroDeepSeg
+WD=/home/laine/Documents/REPOSITORIES/CCA_DL_TOOLS/caroDeepSeg
 cd $WD
 PYTHONPATH=$WD python package_cores/run_training.py -param set_parameters_training.py
