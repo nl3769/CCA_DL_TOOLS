b3d1818d52d78829dfc703070eff24bb0a118153
laine <nolann.laine@outlook.fr>
2023-02-21 09:04:40 +0100

    add run_local shell

--- caroDeepMotion/run_local/run_database_preparation.sh
+++ caroDeepMotion/run_local/run_database_preparation.sh
@@ -3,4 +3,4 @@
 source ~/venv/pytorch/bin/activate
 
 WD=/home/laine/Documents/REPOSITORIES/CCA_DL_TOOLS/caroDeepMotion
-PYTHONPATH=$WD python -m pudb package_core/run_flyingChairs_handler.py -param set_parameters_flyingChairHandler.py
+PYTHONPATH=$WD python package_core/run_database_preparation.py -param set_parameters_database.py
